e20814cccf2da80c7537fb6051658ded1e663f1c
isse <sanchezclarisse@gmail.com>
2016-10-15 17:25:26 +0800

    feat: add Google Cloud Endpoint

---
+++ backend/src/main/java/com/example/isse/myapplication/backend/MyJoke.java
@@ -0,0 +1,17 @@
+package com.example.isse.myapplication.backend;
+
+/**
+ * Created by isse on 04/10/2016.
+ */
+
+public class MyJoke {
+    private String joke;
+
+    public String getJoke() {
+        return joke;
+    }
+
+    public void setJoke(String joke) {
+        this.joke = joke;
+    }
+}
