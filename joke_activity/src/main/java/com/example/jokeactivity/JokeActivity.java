bcf4a88c95300f6c32c501b8e6bafeee86b2c85e
isse <sanchezclarisse@gmail.com>
2016-10-17 15:08:12 +0800

    feat: add manifest and activity for android library

---
+++ joke_activity/src/main/java/com/example/jokeactivity/JokeActivity.java
@@ -0,0 +1,24 @@
+package com.example.jokeactivity;
+
+import android.net.Uri;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class JokeActivity extends ActionBarActivity implements JokeFragment.OnFragmentInteractionListener {
+
+    public static String joke = "joke";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_joke);
+
+
+    }
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+
+    }
+}
