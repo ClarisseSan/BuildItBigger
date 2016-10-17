31a860810bd2198272051f7428316f404f1dfd03
isse <sanchezclarisse@gmail.com>
2016-10-17 16:05:58 +0800

    test: add code to test that Asynctask successfully retrieves a non-empty string

--- app/src/androidTest/java/com/udacity/gradle/builditbigger/ApplicationTest.java
+++ app/src/androidTest/java/com/udacity/gradle/builditbigger/ApplicationTest.java
@@ -1,13 +1,46 @@
 package com.udacity.gradle.builditbigger;
+import android.content.Context;
+import android.support.test.InstrumentationRegistry;
+import android.support.test.runner.AndroidJUnit4;
+import android.util.Log;
 
-import android.app.Application;
-import android.test.ApplicationTestCase;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.*;
 
 /**
- * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ * Instrumentation test, which will execute on an Android device.
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
-public class ApplicationTest extends ApplicationTestCase<Application> {
-    public ApplicationTest() {
-        super(Application.class);
+@RunWith(AndroidJUnit4.class)
+public class ApplicationTest {
+    @Test
+    public void useAppContext() throws Exception {
+        // Context of the app under test.
+        Context appContext = InstrumentationRegistry.getTargetContext();
+
+        Log.e("CONTEXT", appContext.toString());
+
+        assertEquals("com.udacity.gradle.builditbigger", appContext.getPackageName());
     }
+
+    @Test
+    public void testASynctask() throws Exception {
+        // Context of the app under test.
+        Context mContext = InstrumentationRegistry.getContext();
+
+        EndpointAsyncTask endpointAsyncTask = new EndpointAsyncTask(mContext, new EndpointAsyncTask.AsyncResponse() {
+            @Override
+            public void processFinish(String output) {
+
+                assertNotNull(output);
+            }
+        });
+        endpointAsyncTask.execute();
+
+    }
+
+
 }
